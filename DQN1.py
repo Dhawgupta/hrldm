9ab0fceb6b75fb97371c0ff5544b1a8e793d89ea
Dhawal Gupta <dhawal.gupta.iitp@gmakl.com>
2019-02-09 20:59:14 +0530

    Adding the python files

--- DQN1.py
+++ DQN1.py
@@ -14,6 +14,21 @@ from keras.models import load_model
 import time
 
 from SumTree1 import SumTree
+import tensorflow as tf
+from keras import backend as k
+
+###################################
+# TensorFlow wizardry
+config = tf.ConfigProto()
+
+# Don't pre-allocate memory; allocate as-needed
+config.gpu_options.allow_growth = True
+
+# Only allow a total of half the GPU memory to be allocated
+config.gpu_options.per_process_gpu_memory_fraction = 0.2
+
+
+# Create a session with the above options specified.
 
 
 class Memory:   # stored as ( s, a, r, s_ ) in SumTree
